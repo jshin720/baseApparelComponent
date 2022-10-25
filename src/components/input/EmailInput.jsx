82f0ce448fea50bb6c3b3d04e6f746fe468a113b
Jshin720 <jeffshin720@gmail.com>
2022-10-25 18:33:49 -0400

    error handling works

--- src/components/input/EmailInput.jsx
+++ src/components/input/EmailInput.jsx
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from 'react'
 
 function EmailInput() {
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("")
+
 
   const isValidEmail = (email) => {
-    return setError(/\S+@\S+\.\S+/.test(email.target.value));
+    return setError(/\S+@\S+\.\S+/.test(email));
   };
 
   const handleChange = (e) => {
-    console.log("e", e)
-    if (!isValidEmail(e.target.value)) {
-      setMessage("Email is Invalid")
-    } 
+    setEmail(e.target.value);
+    if (!isValidEmail(email)) {
+        setMessage("Email is Invalid")
+    }
   }
-  
-  console.log("error", error)
-  console.log("messgae", message)
+    
+   console.log("e", email)
+ 
+    console.log("error", error)
+  // console.log("messgae", message)
 
   return (
     <>
     <div className="email-container">
-      <input type="email" className="email-form" value="email" onChange={handleChange}/>
+      <input type="email" className="email-form" value={email} onChange={handleChange} placeholder="email"/>
       {error ? <h4 style={{color: "red"}}>{message} </h4>  : null}
     </div>
 
