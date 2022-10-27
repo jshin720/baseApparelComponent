16e94462686d9fc28e397e7a813445d10f846bb0
Jshin720 <jeffshin720@gmail.com>
2022-10-27 16:12:03 -0400

    added a submit button

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,39 +1,40 @@
 import React from "react";
-import bALogo from "../../images/logo.svg"
+import bALogo from "../../images/logo.svg";
 import EmailInput from "../input/EmailInput";
-import desktopImg from "../../images/hero-desktop.jpg"
-import mobileImg from "../../images/hero-mobile.jpg"
+import desktopImg from "../../images/hero-desktop.jpg";
+import mobileImg from "../../images/hero-mobile.jpg";
 
 function Card() {
   return (
     <div className="main-card-container">
-      <div className="main-img">
-        <img
-          src={desktopImg}
-          alt="desktop"
-          className="desktop-img"
-        />
-        <img
-          src={mobileImg}
-          alt="mobile"
-          className="mobile-img"
-        />
-      </div>
       <main className="card-container">
-        <span className="logo">
-          <img src={bALogo} alt="logo" />
-        </span>
-        <div className="description-container">
-          <h1 className="coming-soon-container">We're coming soon</h1>
-          <p>
-            Hello fellow shoppers! We're currently building our new fashion
-            store. Add your email below to stay up-to-date with announcements
-            and our launch deals.
-          </p>
-        </div>
-        <div>
-          <EmailInput/>
-        </div>
+        <article>
+          <picture className="main-img">
+            <img src={desktopImg} alt="desktop" className="desktop-img" />
+            <source
+              media="(min-width: 768px)"
+              srcset={mobileImg}
+              alt="mobile"
+              className="mobile-img"
+            />
+          </picture>
+        </article>
+        <article>
+          <span className="logo">
+            <img src={bALogo} alt="logo" />
+          </span>
+          <div className="description-container">
+            <h1 className="coming-soon-container">We're coming soon</h1>
+            <p>
+              Hello fellow shoppers! We're currently building our new fashion
+              store. Add your email below to stay up-to-date with announcements
+              and our launch deals.
+            </p>
+          </div>
+          <div>
+            <EmailInput />
+          </div>
+        </article>
       </main>
     </div>
   );
--- src/components/input/EmailInput.jsx
+++ src/components/input/EmailInput.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import arrow from "../../images/icon-arrow.svg";
 import errorIcon from "../../images/icon-error.svg";
 
 function EmailInput() {
   const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
-  const [email, setEmail] = useState("")
-
+  const [email, setEmail] = useState("");
 
   const isValidEmail = (email) => {
     return setError(/\S+@\S+\.\S+/.test(email));
@@ -15,26 +14,34 @@ function EmailInput() {
   const handleChange = (e) => {
     setEmail(e.target.value);
     if (!isValidEmail(email)) {
-        setMessage("Email is Invalid")
+      setMessage("Email is Invalid");
     }
-  }
-    
-   console.log("e", email)
- 
-    console.log("error", error)
+  };
+
+  console.log("e", email);
+
+  console.log("error", error);
   // console.log("messgae", message)
 
   return (
     <>
-    <div className="email-container">
-      <input type="email" className="email-form" value={email} onChange={handleChange} placeholder="email"/>
-      <img src={arrow} alt="" />
-      <img src={errorIcon} alt="" />
-      {error ? <h4 style={{color: "red"}}>{message} </h4>  : null}
-    </div>
-
+      <div className="email-container">
+        <input
+          type="email"
+          className="email-form"
+          value={email}
+          onChange={handleChange}
+          placeholder="Email Address"
+          required
+        />
+        <button type="submit">
+          <img src={arrow} alt="" />
+        </button>
+        <img src={errorIcon} alt="" />
+        {error ? <h4 style={{ color: "red" }}>{message} </h4> : null}
+      </div>
     </>
-  )
+  );
 }
 
 export default EmailInput;
