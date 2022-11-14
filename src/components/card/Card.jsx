30a6f96ea0573686355ee08bac6721859d55680b
Jshin720 <jeffshin720@gmail.com>
2022-11-14 14:45:04 -0500

    worked on the css for the paragraphs on the card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,43 +3,42 @@ import bALogo from "../../images/logo.svg";
 import EmailInput from "../input/EmailInput";
 import desktopImg from "../../images/hero-desktop.jpg";
 import mobileImg from "../../images/hero-mobile.jpg";
-import "./Card.css"
+import "./Card.css";
 
 function Card() {
   return (
     <div className="main-card-container">
-      <main className="card-container">
-        <article className="left">
-          <span className="logo">
-            <img src={bALogo} alt="logo" />
-          </span>
-          <div className="description-container">
-            <h1 className="coming-soon-container">
-              <span>We're</span>
-              coming soon
-            </h1>
-            <p>
-              Hello fellow shoppers! We're currently building our new fashion
-              store. Add your email below to stay up-to-date with announcements
-              and our launch deals.
-            </p>
-          </div>
-          <div>
-            <EmailInput />
-          </div>
-        </article>
-        <article className="right">
-          <picture className="main-img">
-            <img src={desktopImg} alt="desktop" className="desktop-img" />
-            <source
-              media="(min-width: 768px)"
-              srcset={mobileImg}
-              alt="mobile"
-              className="mobile-img"
-            />
-          </picture>
-        </article>
-      </main>
+      <article className="left">
+        <img src={bALogo} alt="logo" className="logo" />
+        <div className="description-container">
+          <h1 className="header">
+            <span className="were-group">We're</span>
+            <span className="cs-group">
+              <br />
+              coming <br /> soon
+            </span>
+          </h1>
+          <p className="intro">
+            Hello fellow shoppers! We're currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+        </div>
+        <div>
+          <EmailInput />
+        </div>
+      </article>
+      <article className="right">
+        <picture className="main-img">
+          <img src={desktopImg} alt="desktop" className="desktop-img" />
+          <source
+            media="(min-width: 768px)"
+            srcset={mobileImg}
+            alt="mobile"
+            className="mobile-img"
+          />
+        </picture>
+      </article>
     </div>
   );
 }
